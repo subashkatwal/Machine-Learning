f52de1a8c3972613f3d4ffc1d9ce897f22ef7fad
Subash Katwal <subashkatwal112@gmail.com>
2025-06-23 08:14:53 +0545

    initial commit

--- HeartDiseasePrediction/heartdisease.py
+++ HeartDiseasePrediction/heartdisease.py
@@ -2,25 +2,28 @@ import pandas as pd
 import pylab as plt 
 import numpy as np 
 import scipy.optimize as opt 
+from sklearn.discriminant_analysis import StandardScaler
 import statsmodels.api as sm 
 from sklearn import preprocessing
 import matplotlib.pyplot as plt 
 import seaborn as sns 
 
 
-diseases_df = pd.read_csv(r'C:\Users\Dell\Desktop\Machine Learning\ML.py\HeartDiseasePrediction\framingham.csv')
+diseases_df = pd.read_csv('C:/Users/Dell/Desktop/ML/Machine-Learning/HeartDiseasePrediction/framingham.csv')
+
+# print(diseases_df.head(5))
 
 diseases_df.drop(['education'],inplace=True , axis=1)
 diseases_df.rename(columns={'male':'Sex_male'},inplace=True)
-print(diseases_df)
+# print(diseases_df)
 
-'''removing NaN / NULL values'''
+# '''removing NaN / NULL values'''
 diseases_df.dropna(axis=0,inplace=True)
 print(diseases_df.head(),diseases_df.shape) #.shape determines the dimension of rows and columns
 print(diseases_df.TenYearCHD.value_counts())# value_counts calculate the frequency of each unique value in column
 
 
-#splitting data into Dataset (Test and Train )
+# #splitting data into Dataset (Test and Train )
 
 X=np.asarray(diseases_df[['age','Sex_male','cigsPerDay','totChol', 'sysBP', 'glucose',]])
 y=np.asarray(diseases_df['TenYearCHD'])
@@ -30,15 +33,15 @@ x=preprocessing.StandardScaler().fit(X).transform(X)
 #Train & Test 
 from sklearn.model_selection import train_test_split 
 X_train,X_test,Y_train,Y_test=train_test_split(
-    X,y,test_size=0.3,random_state=4
+    X,y,test_size=0.3,random_state=42
 )
 
 print('Train set :',X_train.shape,Y_train.shape)
 print('Test set :',X_test.shape,Y_test.shape)
 
-# plt.figure(figsize=(7,5))
-# sns.countplot(x='TenYearCHD',data=diseases_df)
-# plt.show()
+plt.figure(figsize=(7,5))
+sns.countplot(x='TenYearCHD',data=diseases_df)
+plt.show()
 
 '''Counting no of people affected bu CHD where 0--> not affected & 1-> affected'''
 laste= diseases_df['TenYearCHD'].plot()
@@ -66,3 +69,30 @@ plt.show()
 
 print('The details for confusion matrix is = ')
 print(classification_report(Y_test,y_Pred))
+
+age = int(input("Enter age: "))
+sex = int(input("Enter Sex (1 = Male, 0 = Female): "))
+cigs = float(input("Cigs per day: "))
+chol = float(input("Total cholesterol: "))
+bp = float(input("Systolic BP: "))
+glucose = float(input("Glucose level: "))
+
+input_df = pd.DataFrame([{
+    "age": age,
+    "Sex_male": sex,
+    "cigsPerDay": cigs,
+    "totChol": chol,
+    "sysBP": bp,
+    "glucose": glucose
+}])
+
+new_data= pd.DataFrame(input_df)
+scaler=preprocessing.StandardScaler().fit(X)
+scaled_input= scaler.transform(new_data)
+
+prediction=logReg.predict(scaled_input)
+
+print(" Prediction for new user : \n ")
+print("Input :",new_data)
+
+print("Predicted Risk (TenYearCHD):", int(prediction[0]), "→", "Risk" if prediction[0] == 1 else "No Risk")
